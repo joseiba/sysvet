d7a6263dfa45d0acf64d6fd736f7599d8091dbd4
joseiba <familiajoselo@gmail.com>
2020-06-09 21:14:45 -0400

    add back-end to project

---
+++ back-end/src/main/java/com/sysvet/PplServerApplication.java
@@ -0,0 +1,13 @@
+package com.sysvet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PplServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PplServerApplication.class, args);
+	}
+
+}
